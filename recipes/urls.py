a23a5f278f1ff2894a3b8dfaf122584a79f4f89a
Leonardo22sp <santos.leonardo22sp@gmail.com>
2025-08-06 21:30:11 -0300

    adicao de receitas e imagens das receitas

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,10 @@
+# recipes/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
+    # Detalhe da receita pelo ID
+    path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    # Outras URLs do seu aplicativo podem ser adicionadas aqui
 ]
